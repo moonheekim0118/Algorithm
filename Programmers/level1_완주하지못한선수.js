65e1fc3492d6e682f2e9173a31ce2734d6c218e8
moonee <uuuu118@naver.com>
2021-01-30 13:38:43 +0900

    add 20210130

---
+++ Programmers/level1_완주하지못한선수.js
@@ -0,0 +1,13 @@
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
+
+    for(let i =0; i < participant.length ; i++){
+        if(completion[i]!==participant[i]){
+            return participant[i];
+        }
+    }
+}
+
+console.log(
+    solution(["leo", "kiki", "eden"],["eden", "kiki"]));
