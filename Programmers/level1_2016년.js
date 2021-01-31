0130bedaad8e17f18d74e65d421668cc3300cfeb
moonee <uuuu118@naver.com>
2021-01-31 12:13:38 +0900

    refactor code

--- Programmers/level1_2016년.js
+++ Programmers/level1_2016년.js
@@ -1,4 +1,7 @@
 
+const LEAP=[0,31,29,31,30,31,30,31,31,30,31,30,31];
+const COMMON=[0,31,28,31,30,31,30,31,31,30,31,30,31];
+
 // 1월 1일의 요일을 넣어주면 알맞는 요일표를 반환
 function getDAYS(startDay){
     const DAY=['MON','TUE','WED','THU','FRI','SAT','SUN'];
@@ -12,12 +15,13 @@ function getDAYS(startDay){
     return SpecificDays;
 }
 
-function solution(a, b,startDay='FRI') {
+function solution(a, b,startDay='FRI',isLeap=true) {
     const DAY=getDAYS(startDay);
-    const totalDates=[0,31,29,31,30,31,30,31,31,30,31,30,31]
-    .splice(a===1?0:1,a===1?1:a-1).reduce((accumulator, value)=>accumulator+value)+b;
+    const MONTHS = isLeap ? LEAP : COMMON;
+    const totalDates=MONTHS.splice(a===1?0:1,a===1?1:a-1).reduce((accumulator, value)=>accumulator+value)+b;
+    
 
     return DAY[totalDates%7-1===-1?6:totalDates%7-1];
 }
 
-console.log(solution(1,6));
+console.log(solution(1,1));
